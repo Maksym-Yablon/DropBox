9e91922d56b327a1a8c534cc6fb419008dffdf47
maksimelyann1 <yablonskiymaksim7@gmail.com>
2025-07-14 15:48:18 +0300

    в1.3

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
 import pygame
 from sys import exit
 
+# Константи
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+# Розміри екрану
+SCREEN_WIDTH = 650
+SCREEN_HEIGHT = 650
+
+BACKGROUND_IMAGE = pygame.transform.scale(pygame.image.load("image/icon.png"),(SCREEN_WIDTH, SCREEN_HEIGHT)) #картинка фону, з маштабуванням під розмір екрану
+
+
+# Ініціалізація Pygame
+pygame.display.set_caption("Drop Box")
+pygame.display.set_icon(pygame.image.load("image/icon.png"))
 
 pygame.init()
-screen = pygame.display.set_mode((650,650))
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 clock = pygame.time.Clock()
+
+screen.blit(BACKGROUND_IMAGE, (0, 0))
 
 
 while True:
